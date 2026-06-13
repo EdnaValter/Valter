546708463a8275efc1e1737311e0144f2c1e55a2
Edna Valter <edna@valter.dk>
2026-06-13 14:33:21 +0200

    Add Sites deployment setup

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  appType: 'mpa',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        services: resolve(__dirname, 'services.html'),
+        contact: resolve(__dirname, 'contact.html'),
+      },
+    },
+  },
+});
